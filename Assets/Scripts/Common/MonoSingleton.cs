745238acbd087728bf09477e259fd9f1765828b3
sakaitoru <sakaitorugame@gmail.com>
2019-07-12 14:00:53 +0900

    MonoSingleton追加

---
+++ Assets/Scripts/Common/MonoSingleton.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 継承したクラスのStartでDontDestroyonLoad(gameObject)
+/// をするとゲーム全体でのシングルトンができる．
+/// しないとシーン内でのシングルトンができる．
+/// 継承先ではAwake,OnDestroyは使えないので，
+/// SubAwake,SubOnDestroyを使う.
+/// public class 継承クラス:MonoSingleton<継承クラス>って感じで使う.
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public abstract class MonoSingleton<T> :
+    MonoBehaviour where T : MonoSingleton<T>
+{
+
+    /// <summary>
+    /// すでにインスタンスがあれば破棄
+    /// 値をシーンごとにコピーする場合はこれをオーバーライド
+    /// </summary>
+    protected virtual void Awake()
+    {
+        if (_Instance != null)
+        {
+            print(typeof(T) + "was instantiated");
+            Destroy(gameObject);
+            return;
+        }
+        Instance = (T)this;
+        SubAwake();
+    }
+
+    protected virtual void SubAwake()
+    {
+
+    }
+
+    protected virtual void OnDestroy()
+    {
+        SubOnDestroy();
+        if (Instance == this)
+        {
+            Instance = null;
+        }
+    }
+
+    protected virtual void SubOnDestroy()
+    {
+    }
+
+    public static T Instance
+    {
+        get
+        {
+            if (_Instance == null)
+            {
+                Debug.LogWarning(typeof(T) + "is not instantiated");
+            }
+            return _Instance;
+        }
+        protected set
+        {
+            _Instance = value;
+        }
+    }
+
+    private static T _Instance;
+}
