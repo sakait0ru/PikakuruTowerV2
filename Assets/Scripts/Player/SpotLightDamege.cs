aa4caef869e52940ae2e31855b2d5861e64ae72a
sakaitoru <sakaitorugame@gmail.com>
2020-01-20 12:53:08 +0900

    Enemyのプレハブ作成、ステージ調整

---
+++ Assets/Scripts/Player/SpotLightDamege.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpotLightDamege : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    // 触れた時
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            other.gameObject.GetComponent<EnemyHp>().Damege();
+        }
+    }
+    
+    // 触れている間
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            other.gameObject.GetComponent<EnemyHp>().Damege();
+        }
+    }
+}
