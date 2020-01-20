e04f7ee13bdd5139eedf1c9943475f6b4f7e78c1
sakaitoru <sakaitorugame@gmail.com>
2020-01-20 12:52:18 +0900

    Enemyがタワーに向かって動く処理追加

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 中心に寄ってくる　角度もタワーの方向を向く
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 m_velocity;
+
+    [SerializeField]
+    private Transform m_target;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // 移動
+        m_velocity += (m_target.position - transform.position);
+        m_velocity *= 0.25f;
+        m_velocity.y = 0;
+        transform.position += m_velocity *= Time.deltaTime;
+
+        // 向き
+        var aim = m_target.position - this.transform.position;
+        var look = Quaternion.LookRotation(aim);
+        this.transform.localRotation = look;
+    }
+}
