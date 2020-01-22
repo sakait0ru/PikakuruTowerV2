0f904866c612a5cb7373af8b242ed7dc08968af5
sakaitoru <sakaitorugame@gmail.com>
2020-01-22 10:07:47 +0900

    ジャイロのコントローラ追加

---
+++ Assets/Scripts/Player/GyroController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// ここにジャイロの取得を記述
+
+public class GyroController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
