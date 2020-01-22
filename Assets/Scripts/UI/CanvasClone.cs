247bec0d8dd244e23b06cdafb49f868bd1fc8d64
sakaitoru <sakaitorugame@gmail.com>
2020-01-22 13:32:25 +0900

    UIプレハブを生成するスクリプト追加

---
+++ Assets/Scripts/UI/CanvasClone.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasClone : MonoBehaviour
+{
+    // 複製するcanvas
+    public GameObject canvas;
+
+    // 設定するカメラ（Right）
+    public Camera tagetCamera;
+
+    private void Awake()
+    {
+        GameObject rCanvas = Instantiate(canvas);
+        Canvas canvasData = rCanvas.GetComponent<Canvas>();
+        canvasData.worldCamera = tagetCamera;
+    }
+}
--- Assets/Scripts/UI/StartPanel.cs
+++ Assets/Scripts/UI/StartPanel.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class StartPanel : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GyroController gyroCon;
+
     void Start()
     {
-        
+        if (!gyroCon)
+        {
+            //何も設定さていなかった時は取得
+            gyroCon = GameObject.Find("CameraController").GetComponent<GyroController>();
+
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Aボタンを押した時にする
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            Destroy(this.gameObject);
+
+            // ジャイロの角度保存
+            //gyroCon.
+        }
     }
 }
